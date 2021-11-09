c0e0f2c4cfc60fd9fcd001da89a41ae03ca414ea
mrfaeez <faexd99@gmail.com>
2021-11-09 23:55:59 +0800

    add stateful

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My First Ap',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'My First App'),
@@ -49,14 +40,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var questionIndex = 0;
   void buttonPressed() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
     print('Button ' + questionIndex.toString() + ' Pressed!');
   }
 
-  var questions = {'Question 1', 'Question 2', 'Question 3', 'Question 4'};
-
   @override
   Widget build(BuildContext context) {
+    var questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4'];
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
